4e062056743889199495211a8d5765cc48430581
PedFelGS <pedfelgs@gmail.com>
2023-12-02 10:21:30 -0300

    atualizando mod userController

--- Project Professor/Controller/userController.js
+++ Project Professor/Controller/userController.js
@@ -1,11 +1,14 @@
 const connection = require('../database') //insert query que será adicionado ao banco de dados para inserir o user ao banco
 
 class userController{
-	async create(req,res){ //função assícrona // é necessáriaela para se connectar com o banco de dados
-		const {nome, email, senha} = req.body
-	}
+	async create(req,res){
+    //função assícrona // é necessáriaela para se connectar com o banco de dados
+    const { nome, email, senha } = req.body
 
-	const sql = 'INSERT INTO'
+    const sql = "INSERT INTO users (nome, email, senha) VALUES ( ?, ?, ?)" // fazendo insert // será necessário um método no user controller para exibir os usuários cadastrados, será necessário o select
+
+		const values = [nome, email, senha] 
+  }
 }
 
 module.exports = userController
