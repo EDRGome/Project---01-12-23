5f0b6179ca0fcfc0bcfe85ef55d4979664de086e
PedFelGS <pedfelgs@gmail.com>
2023-12-02 10:39:34 -0300

    Atualizando mod userController - finalizado 1 parte

--- Project Professor/Controller/userController.js
+++ Project Professor/Controller/userController.js
@@ -3,11 +3,20 @@ const connection = require('../database') //insert query que será adicionado ao
 class userController{
 	async create(req,res){
     //função assícrona // é necessáriaela para se connectar com o banco de dados
-    const { nome, email, senha } = req.body
+    const { nome, email, senha } = req.body //
 
     const sql = "INSERT INTO users (nome, email, senha) VALUES ( ?, ?, ?)" // fazendo insert // será necessário um método no user controller para exibir os usuários cadastrados, será necessário o select
 
 		const values = [nome, email, senha] 
+		connection.query(sql, values, (err, results) => { //consulta sql
+			if(err) {
+				console.error("Erro ao inserir o usuário.")
+				//res.send()
+			} else{
+				console.log("Usuário cadastrado com sucesso!")
+				res.json({message: "Usuario cadastrado com sucesso!"})
+			}
+		})
   }
 }
 
--- notes.md
+++ notes.md
@@ -1,1 +1,9 @@
 Formas de exportar algo de dentro de um módulo NODE para ser usado em outro arquivo
+
+
+npm init -y
+npm i express
+npm i nodemon
+npm i mysql2
+npm i body-parser
+npm run start
