41b922d9b1d3eb731e6d1a175cdf29fff470b727
PedFelGS <pedfelgs@gmail.com>
2023-12-09 10:52:40 -0300

    Criando User - Senha Criptograda

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,13 +1,21 @@
+const bcrypt = require("bcryptjs")
 const connection = require("../database")
+const AppError = require("../AppError")
 
 class userController {
   async create(req, res) {
     const { id_users, name_users, email, password_users } = req.body //
 
+    const senhaCriptografada = await bcrypt.hash(password_users, 8)
+
+    if (!name_users) {
+      throw new AppError("O nome é obrigatório!")
+    }
+
     const sql =
       "INSERT INTO users (id_users, name_users, email, password_users) VALUES (?, ?, ?, ?)"
 
-    const values = [id_users, name_users, email, password_users]
+    const values = [id_users, name_users, email, senhaCriptografada]
 
     connection.query(sql, values, (err, results) => {
       if (err) {
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const connection = require("./database")
 const Route = require("./Routes")
+require("express-async-errors")
+const AppError = require("./AppError")
 
 const app = express()
+
+app.use((error, req, res, next) => {
+	if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    })
+  }
+  console.error(error)
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  })
+}) // o que é middleware
+
 
 app.use(express.json())
 app.use(Route)
@@ -20,6 +37,6 @@ app.get("/", (req,res) => ){
 	console.log
 }
 */
-app.listen(3000, () => {
+app.listen(3033, () => {
   console.log("Servidor rodando na porta 3000")
 })
